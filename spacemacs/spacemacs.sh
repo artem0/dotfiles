ee21c7ceb62431d78fa8f4b448200aa9d3504a24
Artem Rukavytsia <artem.rukavitsya@gmail.com>
2019-03-12 01:10:48 +0200

    Spacemacs support

---
+++ spacemacs/spacemacs.sh
@@ -0,0 +1,7 @@
+# Install emacs on macOS
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+brew linkapps emacs-plus
+
+# Install spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
